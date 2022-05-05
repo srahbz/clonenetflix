d6312a525ab82647d783fca3c1e2bcd055bca722
Sarah Benzi <sarahlover822@gmail.com>
2022-05-04 21:59:06 -0300

    array de itens e iformaçoes criado

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -1,2 +1,44 @@
-const API_KEY = '03e6d8505ebc78a15d2bd774e5a48410'
-const API_BASE = 'https://api.themoviedb.org/3'
+const API_KEY = '03e6d8505ebc78a15d2bd774e5a48410';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+export default {
+    getHomeList: async ( ) =>{
+        return[
+            {
+                slug:'originals',
+                title:'Originais da Netflix',
+                items: []
+            },
+            {
+                slug:'trending',
+                title:' Recomendados para Você ',
+                items: []
+            },
+            {
+                slug:'toprated',
+                title:' Em Alta ',
+                items: []
+            },
+            {
+                slug:'action',
+                title:' Ação ',
+                items: []
+            },
+            {
+                slug:'comedy',
+                title:' Comedia ',
+                items: []
+            },
+            {
+                slug:'horror',
+                title:' terror ',
+                items: []
+            },
+            {
+                slug:'documentary',
+                title:' Documentarios ',
+                items: []
+            },
+        ]
+    }
+}
