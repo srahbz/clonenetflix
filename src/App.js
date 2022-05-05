b554b05dbb8e1ca4f4f96a3cc9eebbb297fb562d
Sarah Benzi <sarahlover822@gmail.com>
2022-05-05 08:46:33 -0300

    implementada constante que salva lista para poder ser exibida

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Tmdb from "./Tmdb";
 
 export default () => {
 
+  const [movieList,setMovieList] = useState([])
+
   useEffect(() => {
     const loadAll = async() => {
       // pegando a lista total
       let list = await Tmdb.getHomeList();
-      console.log(list);
+      setMovieList(list)
     }
 
     loadAll();
